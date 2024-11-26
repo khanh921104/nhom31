bcba22de68e92b0217c1e20682d53667d93e594b
khanh921104 <lecongkhanh54264@gmail.com>
2024-11-26 22:52:49 +0700

    first

---
+++ API/Data/tbBookGhe.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class tbBookGhe
+    {
+        [Key, Column(Order = 0)]  // Đánh dấu maGhe là một phần của khóa chính
+        [StringLength(20)]
+        public string MaGhe { get; set; }
+
+        [Key, Column(Order = 1)]  // Đánh dấu maBook là một phần của khóa chính
+        [StringLength(20)]
+        public string MaBook { get; set; }
+
+        // Khóa ngoại - Mối quan hệ với Ghe
+        [ForeignKey("MaGhe")]
+        public virtual tbGhe Ghe { get; set; }
+
+        // Khóa ngoại - Mối quan hệ với BookVe
+        [ForeignKey("MaBook")]
+        public virtual tbBookVe BookVe { get; set; }
+    }
+}
---
+++ API/Model/DbContext.cs
@@ -0,0 +1,38 @@
+﻿using API.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace API.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<tbNhanVien> NhanViens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Định nghĩa index
+            modelBuilder.Entity<tbNhanVien>()
+                .HasIndex(nv => new { nv.TenTK, nv.MatKhau })
+                .IsUnique()
+                .HasDatabaseName("U_nv_user");
+
+            modelBuilder.Entity<tbNhanVien>()
+                .HasIndex(nv => nv.Sdt)
+                .IsUnique()
+                .HasDatabaseName("U_nv_sdt");
+
+            modelBuilder.Entity<tbNhanVien>()
+                .HasIndex(nv => nv.Cccd)
+                .IsUnique()
+                .HasDatabaseName("U_nv_cccd");
+        }
+    }
+}
+
